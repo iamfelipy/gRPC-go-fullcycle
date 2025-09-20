3020218d4f5c4e06d2f8f0dbd35ce5d9f453a82f
Felipy <felipy81br@gmail.com>
2025-09-20 00:15:00 +0000

    feat: implement main function for gRPC server with Category service integration

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,40 @@
+package grpcserver
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/devfullcycle/14-gRPC/internal/database"
+	"github.com/devfullcycle/14-gRPC/internal/pb"
+	"github.com/devfullcycle/14-gRPC/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	categoryService := service.NewCategoryService((*categoryDb))
+
+	grpcServer := grpc.NewServer()
+	// isso foi gerado no /internal/pb/course_category_grpc.pb.go pelo protoc só preciso ligar ele ao grpcServer
+	// atachei o serviço ao servidor grpc
+	// preciso abrir um conexão tcp para que eu consiga falar com o grpc, abrir um porta tcp em go
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
